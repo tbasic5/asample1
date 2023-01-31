13e7437aedfca84d4185fa4ad760315b6f63967e
tbasic5 <tbasicapp@gmail.com>
2023-01-31 15:45:33 +0900

    m hi2

--- src/main/java/com/tech/sosample1/BConroller1.java
+++ src/main/java/com/tech/sosample1/BConroller1.java
@@ -6,6 +6,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class BConroller1 {
 
+
+	@RequestMapping("/hi2")
+	public String hi2() {
+		return "hi/hi2";
+	}
+	
+	
 	@RequestMapping("/hi1")
 	public String hi1() {
 		return "hi/hi1";
